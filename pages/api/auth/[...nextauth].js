63cc176df94d77c80408639b69503de69bbc0f23
lisho <lshevoweb@gmail.com>
2021-08-07 18:53:05 +0200

    abandono de nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,50 +1,75 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 const options = {
   // Configure one or more authentication providers
   providers: [
-    /* Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }), */
+   
     Providers.Credentials({
-      site: 'http://0.0.0.0:4030/api/usuarios/login',
-      pages: { signIn: "/auth/credentials-signin",
-                error: '/auth/error?error=Default',},
-      name: "MiLogueo",
-      credentials: {
+    //site: 'http://0.0.0.0:4030/api/usuarios/login', 
+    
+    pages: {
+      signIn: '/',
+      signOut: '/',
+      error: null, // Error code passed in query string as ?error=
+      verifyRequest: null, // (used for check email message)
+      newUser: null // If set, new users will be directed here on first sign in
+    },
+      /*  credentials: {
         username: { label: "Usuario",
                     type: "text",
                     placeholder: "Nombre de usuario"},
         password: { label: "Password", 
                     type: "password", 
-                    placeholder: "Password"},
+                    placeholder: "Password"}, */
+      /* credentials : {token},  */
+      name: 'Credentials',
+      callbacks: {
+        async signIn(user, account, profile) {
+          console.log("user", user)
+          return true
+        },
+        async redirect(url, baseUrl) {
+          return baseUrl;
+        },
+        async session(session, token) {
+          // Add property to session, like an access_token from a provider.
+          session.accessToken = token.accessToken
+          return session
+        },
+        async jwt(token, user, account, profile, isNewUser) {
+          return token;
+        },
       },
-      
+
       async authorize(credentials, req) {
         // You need to provide your own logic here that takes the credentials
         // submitted and returns either a object representing a user or value
         // that is false/null if the credentials are invalid.
         // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
         // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address) 
-        
-  
+        // (i.e., the request IP address)
+        const user = await fetch("http://0.0.0.0:4030/api/usuarios/login", {
+          method: "POST",
+          body: JSON.stringify(credentials), 
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then(res => console.log(res))
+
         // If no error and we have user data, return it
         if (res.ok && user) {
-          console.log("logueado: ", user)
-          return user
+          console.log("logueado: ", user);
+          return user;
         }
         // Return null if user data could not be retrieved
-        return null
-      }
-    })
+        return null;
+      },
+    }),
   ],
-  session: { jwt:true}
+  session: { jwt: true },
   // A database is optional, but required to persist accounts in a database
   /* database:  */
- 
-}
+};
 
-export default (req, res) => NextAuth(req, res,options);
+export default (req, res) => NextAuth(req, res, options);
